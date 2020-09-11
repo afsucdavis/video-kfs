0cb331406fe91bbfaf2e9646543955fd0bb11153
AFSUCDAVIS <afsucdavis@gmail.com>
2020-09-11 11:29:51 -0700

    all-files

---
+++ story_content/6MMyhshJFko_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.018%20--%3E%2000:00:03.657%0D%0AThe%20new%20%20Kuali%20application%20offers%20a%20tabbed%20interface.%20%0D%0A%0D%0A"}')
---
+++ story_content/6hiCJocXtSv_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:06.104%0D%0AYou%20can%20tell%20that%20a%20navigation%20item%20is%20active%20because%20a%20green%20line%20appears%20next%20to%20the%20tab%20icon.%0D%0A%0D%0A"}')
